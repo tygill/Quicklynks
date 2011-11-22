82c9bcfd0f263d7f0beff58388ff42e9730217d6
Tyler Gill <tyler.gill@byu.edu>
2011-11-21 21:20:22 -0700

    Added database settings and security settings (should still be changed in production, but for testing this should be fine. Database should be named `quicklynks`, user should be `quicklynks` and the password should be `password`.

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '726A706261697265667662x70686576676C666E79475677572626865A6E7387A662E');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '4775766676696726626865617269672');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
