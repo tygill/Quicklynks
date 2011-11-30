1f0053c4c9092ca0cc16e4c96940cea1fa379bf0
nathanwonnacott <nathanwonnacott@gmail.com>
2011-11-30 13:12:34 -0700

    try2

---
+++ app/Controller/LinksController.php
@@ -0,0 +1,12 @@
+<?php
+class UsersController extends AppController {
+    public $helpers = array ('Html','Form');
+    public $name = 'Links';
+    public $uses = array('User', 'Link');
+    
+
+    public function add() {
+		
+    }
+
+}
